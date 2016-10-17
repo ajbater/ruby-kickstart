ea79a702b70ad9e22c8fa6c23e953d006e4e60de
Amanda Bater <amanda22@hotmail.co.uk>
2016-10-17 22:16:31 +0100

    1:2

---
+++ session1/3-challenge/2_arithmetic.rb
@@ -0,0 +1,16 @@
+# fill out the method below, then run the tests with
+#   $ rake 1:2
+
+
+# Given two numbers, a and b, return half of whichever is smallest, as a float
+#
+# arithmetic2(1, 2)    # => 0.5
+# arithmetic2(19, 10)  # => 5.0
+# arithmetic2(-6, -7)  # => -3.5
+
+def arithmetic2(a, b)
+  if a <= b
+    a / 2.to_f
+  else b / 2.to_f
+  end
+end
